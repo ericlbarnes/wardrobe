0b02442fb3238a5aff19be9e738023ad22411230
Eric Barnes <eric@ericlbarnes.com>
2013-05-31 10:17:58 -0400

    Adding migrations

---
+++ app/database/migrations/2013_05_31_140553_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function($table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->text('content');
+			$table->string('image');
+			$table->string('type');
+			$table->tinyInteger('active')->default(0);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('posts');
+	}
+
+}
---
+++ app/database/migrations/2013_05_31_141423_create_tags_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tags', function($table)
+		{
+			$table->integer('post_id');
+			$table->string('tag');
+			$table->unique(array('post_id', 'tag'));
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tags');
+	}
+}
