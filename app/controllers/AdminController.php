8631a820a095d2600497351934cb9094639a8a9b
Taylor Otwell <taylorotwell@gmail.com>
2013-06-03 20:50:11 -0500

    fix typo.:

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -2,25 +2,21 @@
 
 class AdminController extends BaseController {
 
+	/**
+	 * Create a new admin controller instance.
+	 *
+	 * @return void
+	 */
 	public function __construct()
 	{
 		parent::__construct();
-		// $this->beforeFilter('auth');
+
+		$this->beforeFilter('auth');
 	}
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
+	/**
+	 * Get the main admin view.
+	 */
 	public function getIndex()
 	{
 		return View::make('admin.index');
