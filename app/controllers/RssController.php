0776e3515986c607ec2a70a860ef52cf4e135e7f
Eric Barnes <eric@ericlbarnes.com>
2013-06-09 01:32:05 -0400

    Refactoring the rss controller

--- app/controllers/RssController.php
+++ app/controllers/RssController.php
@@ -31,26 +31,15 @@ class RssController extends BaseController {
    */
   public function getIndex()
   {
-    $data = array(
-      'posts'   => $this->posts->active(),
-      'updated' => $this->getUpdatedDate($posts),
-    );
+    $posts = $this->posts->active();
 
-    header("Content-Type: application/rss+xml; charset=UTF-8");
+    $data = array(
+      'posts'   => $posts,
+      'updated' => $posts->first()->atom_date,
+    );
 
     return Response::view('themes.'.$this->theme.'.atom', $data, 200, array(
       'Content-Type' => 'application/rss+xml; charset=UTF-8',
     ));
-  }
-
-  /**
-   * Get the last post publish date to set the feed updated attribute
-   *
-   * @param Illuminate\Database\Eloquent\Collection $posts
-   * @return DateTime
-   */
-  protected function getUpdatedDate($posts)
-  {
-    return $posts[0]->atom_date;
   }
 }
