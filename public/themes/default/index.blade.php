987193b96da024b6ca49d03515804cdd0464761e
Eric Barnes <eric@ericlbarnes.com>
2013-06-13 21:37:56 -0400

    Adjust the theme for new paging

--- public/themes/default/index.blade.php
+++ public/themes/default/index.blade.php
@@ -3,24 +3,15 @@
 @section('content')
 
   <?php $posts = Entries::fetch(array('per_page' => 3)); ?>
-  @foreach($posts as $post)
-    <h3>{{ $post->title }}</h3>
-  @endforeach
 
-  {{ $posts->links() }}
-
-	<section class="home">
-		<div class="hero">
-			<p>Te vidit legimus vulputate his. Ea mea quas putent perpetua. Eius voluptaria ullamcorper cu qui, has in evertitur suscipiantur mediocritatem, duo eirmod commune in. In mollis luptatum has, summo noluisse ius an. In cum ubique virtute atomorum, quodsi eloquentiam eu mea, omnesque hendrerit eu vix. Albucius sadipscing ex his. Sit aeterno principes ex.</p>
-		</div>
-		<h2 class="title">Archives</h2>
-    <ul class="archive">
-      @foreach ($posts as $post)
-        <li>
-          <span>{{ date("M/d/Y", strtotime($post->publish_date)) }}</span> <strong><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></strong>
-        </li>
-      @endforeach
-    </ul>
-		<p><strong><a href="/archive">Complete Archive »</a></strong></p>
+  <section class="home">
+    @foreach ($posts as $post)
+      <div class="post">
+        <h1><a href="/post/{{ $post->slug }}">{{ $post->title }}</a></h1>
+        <div class="date">{{ date("M/d/Y", strtotime($post->publish_date)) }}</div>
+        {{ md($post->content) }}
+      </div>
+    @endforeach
+		{{ $posts->links() }}
 	</section>
 @stop
