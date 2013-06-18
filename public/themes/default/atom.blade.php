8d6f7568d866b15afa8c9f2440942574f8bed1e0
Eric Barnes <eric@ericlbarnes.com>
2013-06-17 20:05:35 -0400

    Fixing rss feed

--- public/themes/default/atom.blade.php
+++ public/themes/default/atom.blade.php
@@ -1,23 +1,23 @@
-{{ '<' }}?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
+{{ '<?xml version="1.0"?>' }}
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>{{ e(site_title()) }}</title>
+    <link>{{ url('/') }}</link>
+    <atom:link href="{{ url('/rss') }}" rel="self" type="application/rss+xml" />
+    <description></description>
+    <copyright>{{ url('/') }}</copyright>
+    <ttl>30</ttl>
 
- <title>{{ site_title() }}</title>
- <link href="{{ Config::get('app.url') }}/rss" rel="self"/>
- <link href="{{ Config::get('app.url') }}"/>
- <updated>{{ $updated }}</updated>
- <id>{{ Config::get('app.url') }}/</id>
-
- @foreach ($posts as $post)
- <entry>
-   <title>{{ $post->title }}</title>
-   <link href="{{ Config::get('app.url') }}/post/{{ $post->slug }}"/>
-   <author>
-      <name>{{ site_title() }}</name>
-    </author>
-   <updated>{{ $post->atom_date }}</updated>
-   <id>{{ Config::get('app.url') }}/post/{{ $post->slug }}</id>
-   <content type="html">{{ md($post->content) }}</content>
- </entry>
- @endforeach
-
-</feed>
+    @foreach ($posts as $post)
+      <item>
+        <title>{{ $post->title }}</title>
+        <description>
+          {{ htmlspecialchars($post->parsed_content) }}
+        </description>
+        <link>{{ url('post/'.$post->slug) }}</link>
+        <guid isPermaLink="true">{{ url('post/'.$post->slug) }}</guid>
+        <pubDate>{{ $post->rss_date }}</pubDate>
+      </item>
+    @endforeach
+  </channel>
+</rss>
