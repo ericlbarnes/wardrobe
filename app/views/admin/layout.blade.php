6d2ea91e9530caf8d75d315a9d7021c77230bf43
Eric Barnes <eric@ericlbarnes.com>
2013-06-28 20:08:57 -0400

    Adding local jquery as a fallback

--- app/views/admin/layout.blade.php
+++ app/views/admin/layout.blade.php
@@ -16,6 +16,7 @@
     </div>
   </div>
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="{{ asset('/admin/js/jquery.js') }} "><\/script>')</script>
   <script type="text/javascript" src="{{ asset('/admin/js/structure.js') }}"></script>
   <script type="text/javascript" src="{{ asset('/admin/js/app.js') }}"></script>
   @yield('footer.js')
