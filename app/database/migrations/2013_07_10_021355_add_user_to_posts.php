acf679f31be8fe700443f01fddbc77ea31e5ee8e
Eric Barnes <eric@ericlbarnes.com>
2013-07-09 22:19:26 -0400

    Adding user to post migration

---
+++ app/database/migrations/2013_07_10_021355_add_user_to_posts.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserToPosts extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('posts', function($table)
+		{
+			$table->integer('user_id')->nullable()->default(1);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('posts', function($table)
+		{
+			$table->dropColumn('user_id');
+		});
+	}
+
+}
