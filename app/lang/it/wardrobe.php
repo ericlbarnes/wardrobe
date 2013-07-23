d67d5fb23bfaa07a3d4d97ad243583c5133aaca7
Michele Somma <djdrake88@gmail.com>
2013-07-23 12:53:11 +0200

    adding italian localization language files

---
+++ app/lang/it/wardrobe.php
@@ -0,0 +1,69 @@
+<?php
+return array(
+
+  // Header
+  'write' => 'Scrivi',
+  'posts' => 'Articoli',
+  'accounts' => 'Accounts',
+  'logout' => 'Logout',
+  'login' => 'Login',
+
+  // Posts
+  'post_title' => 'Titolo',
+  'post_slug' => 'URI Slug',
+  'post_author' => 'Autore',
+  'post_content' => 'Il contenuto va qui...',
+  'post_tags' => 'Tags',
+  'post_scheduled' => 'Programmato',
+  'post_publish_date' => 'Data di pubbicazione',
+  'post_publish_date_set' => 'Imposta',
+  'post_publish' => 'Pubblica Articolo',
+  'post_save' => 'Salva Articolo',
+  'post_preview' => 'Anteprima',
+  'post_expand' => 'Espandi per opzioni',
+  'post_success' => 'Successo!',
+  'post_saved' => 'Articolo correttamente salvato',
+  'post_errors' => '<strong>Errore</strong> Per favore, correggi gli errori',
+
+  // Post List
+  'posts_none' => 'Nessun Articolo',
+  'post_status' => 'Stato',
+  'post_active' => 'Attivo',
+  'post_published' => 'Pubblicato',
+  'post_draft' => 'Bozza',
+  'post_delete' => 'Articolo cancellato',
+  'post_delete_confirm' => 'Sei sicuro di voler cancellare ##post##?',
+
+  // Accounts
+  'account_first_name' => 'Nome',
+  'account_last_name' => 'Cognome',
+  'account_email' => 'Email',
+  'account_password' => 'Password',
+  'account_password_confirm' => 'Conferma Password',
+  'account_password_keep' => 'Lascia questo campo vuoto per mantenere la stessa password',
+  'account_add' => 'Aggiungi Account',
+  'account_save' => 'Salva Account',
+  'account_add_new' => 'Aggiungi un nuovo Account',
+  'account_saved' => '<strong>Successo!</strong> L\'Account è stato salvato!',
+  'account_delete' => 'Cancella Utente',
+  'account_delete_confirm' => 'Sei sicuro di voler cancellare ##first_name## ##last_name##?',
+
+  // Login
+  'login_email' => 'Email',
+  'login_password' => 'Password',
+  'login_remember' => 'Ricordami',
+  'login_sign_in' => 'Accedi',
+  'login_incorrect' => 'La tua email o la password non sono corretti.',
+  'login_forgot' => 'Password dimenticata?',
+
+  // Forgot Password
+  'forgot_password' => 'Password dimenticata',
+  'forgot_sent' => 'Una email con il reset della password è stata inoltrata.',
+  'forgot_send' => 'Invia email di reset password',
+  'forgot_pass_reset' => 'Reset della Password',
+  'forgot_pass_reset_send' => 'Invia email di reset',
+
+  // Errors
+  'error' => 'Errore!',
+  'error_fivehundred' => 'Si è verificato un errore del server.'
+);
